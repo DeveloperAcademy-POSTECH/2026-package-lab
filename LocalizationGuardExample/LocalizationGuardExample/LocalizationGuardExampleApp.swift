741dda1cf9445641f390ceca3921884ddc4209db
sun <sts07190@naver.com>
2026-09-11 23:42:35 +0900

    setting: 프로젝트 생성

---
+++ LocalizationGuardExample/LocalizationGuardExample/LocalizationGuardExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationGuardExampleApp.swift
+//  LocalizationGuardExample
+//
+//  Created by sun on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationGuardExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
